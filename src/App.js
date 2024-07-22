fa0b24e6a7517a13ab42bd181de28ad62558107a
오주리 <ojl98@naver.com>
2024-07-22 22:50:24 +0900

    redux toolkit을 이용하여 slice 생성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import {createStore} from 'redux';
 import {Provider, useSelector, useDispatch} from 'react-redux';
+import {createSlice} from '@reduxjs/toolkit';
+
+//slice : 기능 하나를 위한 작은 store
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: {value:0},
+  reducers:{
+    up:(state, action)=>{
+      //객체의 불변성을 신경쓰면서 코드를 복잡하게 작성할 필요가 없다
+      state.value = state.value + action.step;
+    }
+  }
+});
 
 function reducer(state, action){
   var newState = {...state};
