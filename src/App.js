76d13376efda6fa3c3dcf4c2c13557292cb83aca
오주리 <ojl98@naver.com>
2024-07-22 22:40:38 +0900

    useDispatch 사용하여 reducer에 action 전달

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import {createStore} from 'redux';
-import {Provider, useSelector} from 'react-redux';
+import {Provider, useSelector, useDispatch} from 'react-redux';
 
 function reducer(state, action){
   var newState = {...state};
@@ -12,9 +12,12 @@ const initialState = {value:0}
 const store = createStore(reducer, initialState);
 
 function Counter(){
+  const dispatch = useDispatch();
   const count = useSelector(state => state.value);
   return <div>
-    <button>+</button> {count}
+    <button onClick={()=>{
+      dispatch({type:'UP', step:2});
+    }}>+</button> {count}
   </div>
 }
 
