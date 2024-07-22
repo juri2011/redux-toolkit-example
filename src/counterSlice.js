490a08f80fa81aaac40e7445a4e9658bea23d3ed
오주리 <ojl98@naver.com>
2024-07-22 23:10:48 +0900

    counterSlice 별도의 파일로 분리

---
+++ src/counterSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+//slice : 기능 하나를 위한 작은 store
+const counterSlice = createSlice({
+  name: "counterSlice",
+  initialState: { value: 0 },
+  reducers: {
+    up: (state, action) => {
+      //객체의 불변성을 신경쓰면서 코드를 복잡하게 작성할 필요가 없다
+      //payload는 자동으로 생성된 액션 크리에이터로 값을 전달받을 때 사용됨
+      state.value = state.value + action.payload;
+    },
+  },
+});
+
+export default counterSlice;
